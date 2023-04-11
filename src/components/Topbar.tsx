3cd3d9f568835945e1e6ce4cc213c5248418a26e
Mirko <mirkomassa18@gmail.com>
2023-04-11 17:41:24 +0200

    fixed topbar for mobile

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -3,10 +3,16 @@ import '../App.css';
 function Topbar() {
     return (
         <div className="Topbar">
-            <a href="https://github.com/MirkoMassa">Github</a>
-            <a href="https://www.linkedin.com/in/mirko-massa">Linkedin</a>
+            <img src={require("../images/logo.png")} alt="logo"/>
+            <a href="https://github.com/MirkoMassa">
+                <img id="git" src={require("../images/git.png")} alt="github" />
+            </a>
+            <a href="https://www.linkedin.com/in/mirko-massa">
+                <img id="lnkin" src={require("../images/lnkin.png")} alt="linkedin" />
+            </a>
             <a className="active" href="#home">Portfolio here! (WIP)</a>
         </div>
+        
     );
 }
 
