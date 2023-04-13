e5af3ad35d5a60f4c6519d6bf52bd0bdb3b46048
Mirko <mirko@hathmac.fritz.box>
2023-04-13 17:45:21 +0200

    desktop icons

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -4,13 +4,13 @@ function Topbar() {
     return (
         <div className="Topbar">
             <img src={require("../images/logo.png")} alt="logo"/>
-            <a href="https://github.com/MirkoMassa">
-                <img id="git" src={require("../images/git.png")} alt="github" />
-            </a>
-            <a href="https://www.linkedin.com/in/mirko-massa">
-                <img id="lnkin" src={require("../images/lnkin.png")} alt="linkedin" />
-            </a>
-            <a className="active" href="#home">Portfolio here! (WIP)</a>
+                <a href="https://github.com/MirkoMassa">
+                    <div id="git"></div>
+                </a>
+                <a href="https://www.linkedin.com/in/mirko-massa">
+                    <div id="linkedin"></div>
+                </a>
+            <a className="active" href="home">Portfolio here! (WIP)</a>
         </div>
         
     );
