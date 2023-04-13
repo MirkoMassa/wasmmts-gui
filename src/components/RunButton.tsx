f1c26259f2ee225fafbbb03d27462501be7f5d97
Mirko <mirko@hathmac.fritz.box>
2023-04-13 17:44:15 +0200

    made it a separate component

---
+++ src/components/RunButton.tsx
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function RunButton(props:{run:()=>void}){
+    return(
+        <div className='ButtonDiv'>
+            <button className='RunBtn' onClick={props.run}>RUN</button>
+        </div>
+        
+    )
+}
+
+ export default RunButton;
