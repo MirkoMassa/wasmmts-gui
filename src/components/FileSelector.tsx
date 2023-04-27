acc4165483c6b6643fd867a12b41fc11e12376cb
Mirko <mirkomassa18@gmail.com>
2023-04-27 02:34:57 +0200

    literally one space in the SelectLabel string

--- src/components/FileSelector.tsx
+++ src/components/FileSelector.tsx
@@ -6,7 +6,7 @@ const fileNames = [
 function FileSelector(props:{onChange:(s:string) => void, selected:string}) {
     return (
         <div className="FileSelector">
-            <p className="SelectLabel">Select the .wat file:</p>
+            <p className="SelectLabel">Select the .wat file :</p>
             <select onChange={(event)=> props.onChange(event.target.value)}  value = { props.selected }>
             <option hidden>Select a .wat file...</option>
             {fileNames.map((fileName) => (<option className='OptionFile' key={ fileName }>{ fileName }</option>))}
--- src/components/FileSelectorDesktop.tsx
+++ src/components/FileSelectorDesktop.tsx
@@ -6,7 +6,7 @@ const fileNames = [
 function FileSelectorDesktop(props:{onChange:(s:string) => void, selected:string}) {
     return (
         <div className="FileSelectorDesktop">
-            <p className="SelectLabel">Select the .wat file:</p>
+            <p className="SelectLabel">Select the .wat file :</p>
             <select onChange={(event)=> props.onChange(event.target.value)}  value = { props.selected }>
             <option hidden>Select a .wat file...</option>
             {fileNames.map((fileName) => (<option className='OptionFile' key={ fileName }>{ fileName }</option>))}
