21ec4eb4b976acba4d8c141d1e16f36cde859197
Mirko <mirkomassa18@gmail.com>
2023-05-09 18:14:12 +0200

    Implemented patches view.

---
+++ src/components/MuiPatchesView.tsx
@@ -0,0 +1,62 @@
+import { Collapse, Container, IconButton, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import ExtensionIcon from '@mui/icons-material/Extension';
+import { patchesDescriptor } from 'wasmmts-a_wasm_interpreter/build/src/debugging/stringifier';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+function MuiPatchesView (props:{
+        wasmPatches: patchesDescriptor[],
+        val:number
+        }) {
+
+    const [patchesOpen, setPatchesOpen] = useState(true)
+    function collapseContainer(){
+        setPatchesOpen(!patchesOpen)
+    }
+    
+    let printPatches = () => {
+        return props.wasmPatches[props.val].description.map(desc => 
+            <Typography variant='subtitle1' sx={{
+                border:'2px solid grey',
+                paddingY:'5px',
+              }}>{desc}</Typography>)
+    }
+  return (
+    <Container sx={{
+        paddingY:'16px',
+        maxHeight:'60vh',
+        borderBottom:'1px solid lightgrey',
+    }}>
+        <Typography variant="h5" align='center'
+            className='Titles' 
+            onClick={collapseContainer}
+        >
+        <IconButton onClick={collapseContainer}>
+            <KeyboardArrowDownIcon sx={{
+                display: patchesOpen ? 'inline-block' : 'none'
+            }}
+            />
+            <KeyboardArrowUpIcon sx={{
+                display: patchesOpen ? 'none' : 'inline-block'
+            }}/>
+        </IconButton>
+            <ExtensionIcon fontSize='small'/> Patches <ExtensionIcon fontSize='small'/> 
+         <IconButton onClick={collapseContainer}>
+            <KeyboardArrowDownIcon sx={{
+                display: patchesOpen ? 'inline-block' : 'none'
+            }}
+          />
+            <KeyboardArrowUpIcon sx={{
+                display: patchesOpen ? 'none' : 'inline-block'
+            }}/>
+        </IconButton>
+        </Typography>
+        <Collapse in={patchesOpen} sx={{overflowY:'scroll'}}>
+            {props.wasmPatches.length!==0 && printPatches()}
+        </Collapse>
+    </Container>
+  )
+}
+
+export default MuiPatchesView
