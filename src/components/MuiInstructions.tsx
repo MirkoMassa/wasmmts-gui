aad56fc73b331ad523dec68ebe1a73bb7b34ea30
Mirko <mirkomassa18@gmail.com>
2023-06-01 12:44:54 +0200

    Moved the examples button here in the instructions.

--- src/components/MuiInstructions.tsx
+++ src/components/MuiInstructions.tsx
@@ -1,15 +1,28 @@
-import { Collapse, Container, IconButton, Typography } from '@mui/material';
+import { Collapse, Container, IconButton, Typography, useMediaQuery } from '@mui/material';
 import React, { useState } from 'react'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import FolderOpenIcon from '@mui/icons-material/FolderOpen';
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 
-function MuiInstructions() {
+function MuiInstructions(props:{
+    openExamples:boolean,
+    setOpenExamples: (b:boolean) => void,
+    setOpenStoredFiles: (b:boolean) => void,
+}) {
+
+    const matches = useMediaQuery('(min-width:800px)');
     const [openInstructions, setOpenInstructions] = useState(false);
+    function handleOpenExamples(){
+        if(!props.openExamples){
+            props.setOpenStoredFiles(false);
+          }
+        props.setOpenExamples(!props.openExamples)
+    }
     function collapseContainer(){
         setOpenInstructions(!openInstructions)
       }
+    
     return (
         <Container sx={{paddingY:'10px', borderBottom:'1px solid lightgrey'}}>
             <Typography variant="h6" align='center'
@@ -39,8 +52,16 @@ function MuiInstructions() {
             <Collapse in={openInstructions}>
                 <Typography variant='subtitle1'>
                     In order to start experimenting the debugging features,
-                     try an example function! <br /><br />
-                     <li>Press the <FolderOpenIcon fontSize='small' sx={{color:'#b26a00'}}/> button</li>
+                     try an example function! <br />
+                     {/* EXAMPLES BTN */}
+                    
+                     <li style={{ fontWeight: 'bold' }}>Press this
+                        <IconButton size={matches?'large':'small'} onClick={handleOpenExamples}
+                        sx={{color:'#b26a00'}}
+                        >
+                            Examples<FolderOpenIcon/>
+                        </IconButton>
+                         button!</li>
                      <li>Select  the .wasm file you want to interpret</li>
                      <li>Enter the arguments in the input fields</li>
                      <li>Press the <KeyboardReturnIcon fontSize='small' sx={{color:'#B81414'}}/> button</li>
