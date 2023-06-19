d5e739e64b822033ae5af75b926d08d0ced488fe
MirkoMassa <mirkomassa18@gmail.com>
2023-06-20 00:53:47 +0200

    TsCompile handling, hash checking to avoid injections when retrieving wat and wasm files. Clearing of temporary files.

--- server/server.ts
+++ server/server.ts
@@ -30,7 +30,6 @@ app.get('/', (req, res) => {
 app.post('/webhook', (req, res) => {
   const buildDirectory = '/var/wasmmts-gui';
   // const targetDirectory = '/var/www/mirkomassa.com';
-  console.log ('currend dir',process.cwd());
   const options = {
     cwd: buildDirectory
   };
@@ -60,7 +59,6 @@ app.post('/webhook', (req, res) => {
       res.sendStatus(500);
       return;
     }
-    console.log('pid:',stdout);
   });
 
 });
@@ -76,48 +74,90 @@ app.post('/uploadTemp', (req, res) => {
     if(!Array.isArray(file)){
 
         const hash = createHash('sha256');
-
         hash.update(file.data);
-        console.log(hash.digest('hex'));
+        const digest = hash.digest('hex'); 
 
         file.mv('tempFiles/' + file.name, (err) => {
           if (err) {
             console.log(err)
             return res.status(500).send(err);
           }
-          // console.log('stored', file)
-          res.send('File uploaded successfully');
+          // console.log('hash:',hash)
+          res.send(Buffer.from(digest));
         });
     }
 });
-
-
-  // app.post('/removeTemp', (req, res) => {
-  //   // checking if there are passed files
-  //   if (!req.files || Object.keys(req.files).length === 0) {
-  //     return res.status(400).send('No files were removed.');
-  //   }
-    
-  //   // console.log('file:', file);
-  //   // mv() method saves the file
-  //   file.('tempFiles/' + file.name, (err) => {
-  //     if (err) {
-  //       console.log(err)
-  //       return res.status(500).send(err);
-  //     }
-  //     // console.log('stored', file)
-  //     res.send('File uploaded successfully');
-  //   });
-  // });
-
-
-app.post('/tscompile', (req, res) => {
-  const {fileName} = req.body;
+// after the file, its corresponding hash gets uploaded
+app.post('/uploadHash', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  const { hash } = req.files;
+  const { fileName } = req.body;
   const fileNameNoExt = fileName.slice(0, fileName.length-3);
+  if(!Array.isArray(hash)){
+    hash.mv('tempFiles/' + `${fileNameNoExt}.hash`, (err) => {
+      if (err) {
+        console.log(err)
+        return res.status(500).send(err);
+      }
+      res.sendStatus(200);
+    });
+  }
+});
+
+  app.post('/clearTempFiles', (req, res) => {
+
+    const { filenameNoExt } = req.body;
+    let filePath = `tempFiles/${filenameNoExt}.hash`;
+    fs.unlink(filePath, (error) => {
+      if (error) {
+        console.error(`Error deleting file ${filePath}: ${error}`);
+        return;
+      } else {
+        console.log(`File ${filePath}.hash deleted successfully`);
+      }
+    });
+    filePath = `tempFiles/${filenameNoExt}.wat`;
+    fs.unlink(filePath, (error) => {
+      if (error) {
+        console.error(`Error deleting file ${filePath}: ${error}`);
+        return;
+      } else {
+        console.log(`File ${filePath}.wat deleted successfully`);
+      }
+    });
+    filePath = `tempFiles/${filenameNoExt}.wasm`;
+    fs.unlink(filePath, (error) => {
+      if (error) {
+        console.error(`Error deleting file ${filePath}: ${error}`);
+        return;
+      } else {
+        console.log(`File ${filePath}.wasm deleted successfully`);
+      }
+    });
+    filePath = `tempFiles/${filenameNoExt}.ts`;
+    fs.unlink(filePath, (error) => {
+      if (error) {
+        console.error(`Error deleting file ${filePath}: ${error}`);
+        return;
+      } else {
+        console.log(`File ${filePath}.ts deleted successfully`);
+      }
+    });
+    res.sendStatus(200);
+  });
+
+
+app.post('/tsCompile', (req, res) => {
+
+  const { fileName } = req.body;
+
+  const fileNameNoExt = fileName.replace(/\.[^.]+$/, '');
   const filePath = `tempFiles/${fileName}`;
   const command = 
   `asc ${filePath} -o tempFiles/${fileNameNoExt}.wasm -t tempFiles/${fileNameNoExt}.wat`;
-  console.log('extension', fileNameNoExt, ',path', filePath, ',command', command);
+  // console.log('extension', fileNameNoExt, ',path', filePath, ',command', command);
 
   console.log('compiling', fileName);
   const child = childProcess.exec(command, (error, stdout, stderr) => {
@@ -136,13 +176,24 @@ app.post('/tscompile', (req, res) => {
 });
 
 
-app.get('/compiledFiles', (req, res) => {
+app.post('/compiledFiles', (req, res) => {
   
-  // check the hash!!
-
-  const fileName = req.query.fileName;
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  const { hash } = req.files;
+  const { fileName } = req.body;
+  const fileNameNoExt = fileName.replace(/\.[^.]+$/, '');
+  const storedHash = fs.readFileSync(`tempFiles/${fileNameNoExt}.hash`);
+  // console.log('passed hash', hash)
+  // console.log('storedHash', storedHash);
+  //@ts-ignore
+  if(!Buffer.compare(hash.data, storedHash) === 0){
+    console.error(`Error during file retrieving.`);
+      res.status(500).json({ message: 'Invalid hash.' });
+  }
   if(typeof fileName === 'string'){
-    console.log('getting', fileName);
+    // console.log('getting', fileName);
     const filePath = join(__dirname, 'tempFiles', fileName);
     res.sendFile(filePath);
   }
